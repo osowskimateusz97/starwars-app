7952ab48ab3382b142d4971c62542b7982efeff6
mateusz <osowski.mateuszz@gmail.com>
2020-08-27 11:51:41 +0200

    Updates

---
+++ precache-manifest.2d6b902aec365b3266f35682142ec8a8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2e654622dc774b272585ecfcfda36f",
+    "url": "/starwars-app/index.html"
+  },
+  {
+    "revision": "b8bfb0b31e1673cd30c0",
+    "url": "/starwars-app/static/css/main.ac832c80.chunk.css"
+  },
+  {
+    "revision": "2eaac676821fad79ff09",
+    "url": "/starwars-app/static/js/2.ff4d9e15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starwars-app/static/js/2.ff4d9e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8bfb0b31e1673cd30c0",
+    "url": "/starwars-app/static/js/main.b9de90c4.chunk.js"
+  },
+  {
+    "revision": "ff98ea39d74d9b573452",
+    "url": "/starwars-app/static/js/runtime-main.7c1184be.js"
+  },
+  {
+    "revision": "81e948ad0179ac79be6c28982906dae1",
+    "url": "/starwars-app/static/media/arrowClose.81e948ad.svg"
+  },
+  {
+    "revision": "da45c4c0de0efd2c6e1fd463b04d3b3f",
+    "url": "/starwars-app/static/media/arrowOpen.da45c4c0.svg"
+  },
+  {
+    "revision": "de203a0d51d11479f802bb3e7564abd5",
+    "url": "/starwars-app/static/media/close.de203a0d.svg"
+  },
+  {
+    "revision": "2767e8a23a5fa6ca7436a6bbfd40ea2e",
+    "url": "/starwars-app/static/media/loadingSpinner.2767e8a2.svg"
+  },
+  {
+    "revision": "79977f053d6f42e5e0575fa2c33b24cd",
+    "url": "/starwars-app/static/media/logo.79977f05.svg"
+  },
+  {
+    "revision": "d8b5abddb11a20073189e3c1e56bd14f",
+    "url": "/starwars-app/static/media/search.d8b5abdd.svg"
+  }
+]);
